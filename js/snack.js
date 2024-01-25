91419df37e51918ff71ee437935298a03ec143b5
azizafarooq <azizafarooq2000@gmail.com>
2024-01-25 17:44:20 +0100

    feat: js snack 4

--- js/snack.js
+++ js/snack.js
@@ -38,3 +38,60 @@ for (let i = 1; i < roadBikes.length; i++) {
 
 const { name, weight } = lightBike;
 console.log(`La bici più leggera è ${name} con un peso di ${weight} kg.`);
+
+/********************************************************************************************************************/
+
+// Usa l'array allegato alla traccia per completare i seguenti punti:
+// Valorizzare le proprietà dei punti fatti e dei falli subiti con numeri randomici
+// Creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti, recuperandoli con il destructuring, e stampiamo tutto in console.
+
+const teams = [
+    {
+      name: 'Team Turtle',
+      score: 0,
+      foul: 0,
+    },
+    {
+      name: 'Team Frog',
+      score: 0,
+      foul: 0,
+    },
+    {
+      name: 'Team Penguin',
+      score: 0,
+      foul: 0,
+    },
+    {
+      name: 'Team Hippopotamus',
+      score: 0,
+      foul: 0,
+    },
+    {
+      name: 'Team Seal',
+      score: 0,
+      foul: 0,
+    },
+    {
+      name: 'Team Crocodile',
+      score: 0,
+      foul: 0,
+    },
+    {
+      name: 'Team Dolphin',
+      score: 0,
+      foul: 0,
+    },
+  ];
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * 11); 
+}
+
+for (let i = 0; i < teams.length; i++) {
+    teams[i].score = getRandomNumber(); 
+    teams[i].foul = getRandomNumber(); 
+}
+
+const teamStatistics = teams.map(({ name, foul }) => ({ name, foul }));
+
+console.table(teamStatistics); 
